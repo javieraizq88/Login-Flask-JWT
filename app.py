b0f3b37e7e72e8d7e99e170e1d9d8b5e5457b05b
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-04-15 11:52:42 -0400

    crear usuarios con constraseña y username obligatorios y crea token

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from models import db
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
+from models import db, User
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["ENV"] = "development"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
 
 db.init_app(app)#une la BBDD con la app
-
 Migrate(app, db)
 bcrypt = Bcrypt(app)
 manager = Manager(app)
@@ -23,6 +25,46 @@ CORS(app)
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])# debe ser POST pq voy a mandar info en el body del mensaje a la API
+def register():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON request"}), 400# valido q se esta mandando la info
+
+
+    username = request.json.get("username", None)# le doy un valor por defecto en caso q no venga la variable
+    password = request.json.get("password", None)# le doy un valor por defecto en caso q no venga la variable
+    name = request.json.get("name", " ")# valor opcional
+    lastname = request.json.get("lastname", " ")# valor opcional
+
+    # VALIDACION DEL USERNAME Y PASSWORD Q SON OBLIGATORIOS
+    if not username or username == "":
+        return jsonify({"msg": "Missing username request"}), 400
+    if not password or password == "":
+        return jsonify({"msg": "Missing passwword request"}), 400
+
+    #CREACION DE USUARIO
+    user = User.query.filter_by(username=username).first()# confirma q el usuario sea unico o si ya existe
+    if user:
+        return jsonify({"msg" : "Username all ready exist"}), 400# el usuario ya existe
+
+    user = User()
+    user.username = username
+    user.password = bcrypt.generate_password_hash(password)# genera un TOKEN de la contraseña del usuario para no guardarla textual en la BD
+    user.name = name
+    user.lastname = lastname
+
+    db.session.add(user)
+    db.session.commit()# crea un registro del usuario en la BBDD
+
+    access_token = create_access_token(identity=user.username)# crea un access token y devuelvo un objeto con la info del usuario (username, name y lastname)
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+
+    return jsonify(data), 201# dice q hizo un registro exitoso
+
 
 if __name__ == "__main__":
     manager.run()
--- readme.md
+++ readme.md
@@ -1,17 +1,17 @@
 1) crear archivo Pipfile
 
-2) $ pipenv shell
-para crear el entorno virtual
+2) para crear el entorno virtual
+    $ pipenv shell
+    $ python app.py runserver
 
 3) instalar librerias
-
-$ pipenv install flask
-$ pipenv install flask-script
-$ pipenv install flask-migrate
-$ pipenv install flask-cors
-$ pipenv install flask-sqlalchemy
-$ pipenv install flask-bcrypt ( para encriptar la contraseña)
-$ pipenv install flask-jwr-extended
+    $ pipenv install flask
+    $ pipenv install flask-script
+    $ pipenv install flask-migrate
+    $ pipenv install flask-cors
+    $ pipenv install flask-sqlalchemy
+    $ pipenv install flask-bcrypt ( para encriptar la contraseña)
+    $ pipenv install flask-jwt-extended
 
 4) crear app.py y models.py
     models.py = hace el modelo de la BBDD
@@ -26,5 +26,27 @@ $ pipenv install flask-jwr-extended
 
 9) $ python app.py runserver, arreglar errores q salgan y abrir local host 5000
 
+10) se va a usar 
+    - JWT Manager = para vincular mi app con JWT 
+    - jwt-required = directiva para decir q ruta es privada o no
+    - create_access_token = fx q crea el token para identificar a cada usuario
+    - get_jwt_identity = identifica el usuario q esta logeado 
 
+11) iniciar migraciones
+    $ python app.py db init
+    $ python app.py db migrate
+    $ python app.py db upgrade
 
+12) crear la ruta para el proceso de registro
+
+13) validación de username y password q son obligatorios
+
+14) creacion de usuario creando una instancia en la BBDD
+
+15) usar bcript para no guardar la clave como la manda el usuario 
+
+16) crear q el usuario sea unico o si ya existe
+
+17) crear el token con JWT_SECRET_KEY y create_access_token
+
+18) probar en insmonmia si esta ok el registro
